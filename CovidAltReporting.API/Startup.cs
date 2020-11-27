37494f8a58d78cc232cf6b628d8bfdf137a27d4b
Jeffery Cosby <jeffery.cosby@omes.ok.gov>
2020-11-27 12:06:50 -0600

    Add project files.

---
+++ CovidAltReporting.API/Startup.cs
@@ -0,0 +1,14 @@
+﻿using CovidAltReporting.API;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace CovidAltReporting.API
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
